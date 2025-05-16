10b56de3660de9fdaef5ab1c939523fab654d07a
Gabriela Maia <gabrielamaia.f12@gmail.com>
2025-05-16 17:47:24 -0300

    adicionando pesquisa de satisfação

--- programa.py
+++ programa.py
@@ -1,2 +1,7 @@
 import requests
+
 print("Meu programa é relevante para a aula!")
+
+nota = input("Numa escala de 0 a 5, como você avalia a relevância desse projeto? ")
+
+print(f"Esse projeto tem uma relevância de {nota}")
